7cf94de8c012c03ca66a0cc64fe72c1d6b69426f
autumn-ragland <raglanda998@gmail.com>
2020-03-19 10:25:46 -0500

    conditional rendering

--- conditional-render-lecture-app/src/components/AppContainer.jsx
+++ conditional-render-lecture-app/src/components/AppContainer.jsx
@@ -6,16 +6,36 @@ class AppContainer extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            isLoggingIn : false,
+            isSigningIn : false,
         }
     }
 
+    updateIsLoggingIn = () => {
+        this.setState({isSigningIn : false});
+        this.setState({isLoggingIn : true});
+    }
+
+    updateIsSigningIn = () => {
+        this.setState({isLoggingIn : false});
+        this.setState({isSigningIn : true});
+    }
+
     render(){
+        let userForm;
+        if(this.state.isLoggingIn){
+           // render the log in component
+           userForm = <Login/> 
+        } else if(this.state.isSigningIn){
+            // render the sign up component
+            userForm = <SignUp/>
+        }
         return(
             <div>
                 <h1>20-03-19 React Conditional Rendering Lecture</h1>
-                <Login/>
-                <SignUp/>
+                <button onClick = {this.updateIsLoggingIn}>Log In</button>
+                <button onClick = {this.updateIsSigningIn}>Sign Up</button>
+                {userForm}
             </div>
         )
     }
